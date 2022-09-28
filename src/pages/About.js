6f7b52e82088dafde7bc8a1144138d9ee6773552
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-09-28 20:25:07 +0300

    Add about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,16 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="section section-about">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum vel
+        corrupti architecto doloremque fugiat soluta, quam eius, cupiditate cum
+        voluptatem accusamus unde distinctio quae, odit sequi. Quaerat sint
+        eveniet illo, quia perspiciatis exercitationem quos odit, sed
+        consequatur enim iure vitae!
+      </p>
+    </section>
+  );
 };
 
 export default About;
