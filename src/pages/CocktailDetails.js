dbfc34005a2841ed4e76b66a2bc7987ec0695349
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-09-30 12:26:01 +0300

    Refactor helper function

--- src/pages/CocktailDetails.js
+++ src/pages/CocktailDetails.js
@@ -1,6 +1,40 @@
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { transformData } from '../utils/helpers';
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
 const CocktailDetails = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [cocktail, setCocktail] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const { drinks } = await response.json();
+
+        if (drinks) {
+          const transformedCocktail = transformData(drinks)[0];
+          console.log(transformedCocktail);
+        } else {
+          setCocktail(null);
+        }
+
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+
+    getCocktail();
+  }, [id]);
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,23 +1,25 @@
 export const transformData = (data) => {
   return data.map((drink) => {
-    const {
-      idDrink,
-      strDrink,
-      strCategory,
-      strAlcoholic,
-      strGlass,
-      strInstructions,
-      strDrinkThumb,
-    } = drink;
-
     return {
-      id: idDrink,
-      name: strDrink,
-      image: strDrinkThumb,
-      isAlcoholic: strAlcoholic,
-      glass: strGlass,
-      category: strCategory,
-      instructions: strInstructions,
+      id: drink.idDrink,
+      name: drink.strDrink,
+      image: drink.strDrinkThumb,
+      isAlcoholic: drink.strAlcoholic,
+      glass: drink.strGlass,
+      category: drink.strCategory,
+      instructions: drink.strInstructions,
+      ingredients: [
+        drink.strIngredient1,
+        drink.strIngredient2,
+        drink.strIngredient3,
+        drink.strIngredient4,
+        drink.strIngredient5,
+        drink.strIngredient6,
+        drink.strIngredient7,
+        drink.strIngredient8,
+        drink.strIngredient9,
+        drink.strIngredient10,
+      ],
     };
   });
 };
