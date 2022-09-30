cf1ad6eafafe19682380c4162839b887fc8c0abd
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-09-30 09:31:36 +0300

    Add debounce

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,27 +1,23 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 import { useCocktails } from '../contexts/CocktailsContext';
 
 const SearchForm = () => {
   const { setSearchTerm } = useCocktails();
-  const searchValue = useRef(null);
-  const [isSearching, setIsSearching] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
-    searchValue.current.focus();
-  }, []);
+    if (searchValue) {
+      let searchTimeout = setTimeout(() => {
+        console.log('inside timeout');
+        setSearchTerm(searchValue);
+      }, 500);
 
-  const searchCocktail = () => {
-    // setIsSearching(true);
-    // setIsSearching(false);
-    setSearchTerm(searchValue.current.value);
-
-    //TODO: thing about this carefully and build it properly
-    // setTimeout(() => {
-    //   setIsSearching(false);
-    //   setSearchTerm(searchValue.current.value);
-    // }, 1000);
-  };
+      return () => {
+        clearTimeout(searchTimeout);
+      };
+    }
+  }, [searchValue, setSearchTerm]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -35,21 +31,13 @@ const SearchForm = () => {
             <input
               type="text"
               id="name"
-              ref={searchValue}
-              onChange={searchCocktail}
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
               placeholder="search..."
             />
           </label>
         </div>
       </form>
-      <div
-        className={`${
-          isSearching ? 'form-processing active' : 'form-processing'
-        }`}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          <path d="M304 48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zm0 416c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zM48 304c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48zm464-48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zM142.9 437c18.7-18.7 18.7-49.1 0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9s49.1 18.7 67.9 0zm0-294.2c18.7-18.7 18.7-49.1 0-67.9S93.7 56.2 75 75s-18.7 49.1 0 67.9s49.1 18.7 67.9 0zM369.1 437c18.7 18.7 49.1 18.7 67.9 0s18.7-49.1 0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9z" />
-        </svg>
-      </div>
     </div>
   );
 };
